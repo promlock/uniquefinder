ac69bf610d9b1c0bb1ed3c98bc4c7f4d1474fc0b
kovacsp <kovacsp@email.com>
2023-11-01 20:35:07 +0100

    init project

---
+++ src/main/java/com/example/uniquefinder/UniqueFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.uniquefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqueFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniqueFinderApplication.class, args);
+	}
+
+}
