1bb77f06d5e5b55e5ac0ad489f93e10c3e2d8123
kovacsp <kovacsp@email.com>
2023-11-15 08:50:39 +0100

    add liquibase

--- src/main/resources/db/changelog/changes/V2__create_file_names_table.sql
+++ src/main/resources/db/changelog/changes/V2__create_file_names_table.sql
@@ -1,12 +1,3 @@
-
-create table if not exists request_details(
-  id bigserial not null,
-  folder varchar(100) not null,
-  user_name varchar(100) not null,
-  request_date TIMESTAMP NOT NULL,
-  PRIMARY KEY ( ID )
-);
-
 CREATE TABLE IF NOT EXISTS file_names(
     request_id BIGINT NOT NULL,
     file_name VARCHAR(255) NOT NULL,
